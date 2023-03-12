8df738c8ccb5ecc5001f5594a907a0761472aa06
Tadek <tadeusz.toporkiewicz@gmail.com>
2023-03-12 22:07:14 +0100

    Add Operators += and -= to class Ship. Operators -/+ crew form ship

--- Ship.cpp
+++ Ship.cpp
@@ -2,13 +2,29 @@
    
 
 //operator
+Ship& Ship::operator+=(const int cw){
+    if(cw > maxCrew_ && cw < 0){
+        std::cout<< " Now way, Inccorect number";
+        }
+
+    crew_ += cw;
+    return *this;
+}
+Ship& Ship::operator-=(const int cw){
+    if(cw > maxCrew_ && cw < 0){
+        std::cout<< " Now way, Inccorect number";
+        }
+
+    crew_ -= cw;
+    return *this;
+}
 // consturctors
 
 Ship::Ship(): id_(-1){};
 
 Ship::Ship(std::string name
 ,int id
-,size_t maxCrew
+,int maxCrew
 ,size_t capacity
 ,size_t speed
 ,size_t crew)
@@ -19,6 +35,11 @@ Ship::Ship(std::string name
     ,speed_(speed)
     ,crew_(crew)
     {};
+
+Ship::Ship(int id,
+int maxCrew,
+size_t speed)
+:Ship("Boat",id,maxCrew,50,speed,10){};
 
     /// functions setters  
 void Ship::setName(std::string& n){
--- Ship.h
+++ Ship.h
@@ -10,23 +10,24 @@ class Ship{
     private:
     std::string name_;
     int id_;
-    size_t maxCrew_;
+    int maxCrew_;
     size_t capacity_;
     size_t speed_;
     size_t crew_;
     
     public:
     //operators
-   
+    Ship& operator+=(const int);
+    Ship& operator-=(const int);
     //constructors    
     Ship();
     Ship(std::string name
     ,int id
-    ,size_t maxCrew
+    ,int maxCrew_
     ,size_t capacity
     ,size_t speed
     ,size_t crew);
-
+    Ship(int id, int maxCrew_,size_t speed);
     /// functions setters  
     void setName(std::string& n);
     
