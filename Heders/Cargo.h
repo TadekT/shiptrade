e861653ffb520b76ad63b07874298e689ce4fddc
TadekT <tadeusz.toporkiewicz@gmail.com>
2023-07-06 10:05:53 +0200

    Adding Cargo getters

--- Heders/Cargo.h
+++ Heders/Cargo.h
@@ -5,9 +5,14 @@
 class Cargo
 {
 public:
-
+    std::string const getName();
+    int const getAmount();
+    float const getBasicPrice();
+    int const getCargoWeight();
 //operator 
-    Cargo& operator+=(const int);
+   // Cargo& operator+=(const int addingAmount);
+  //  Cargo& operator-=(const int subtractingAmount);
+
 
 //constructors 
     Cargo();
--- scr/Cargo.cpp
+++ scr/Cargo.cpp
@@ -1,5 +1,10 @@
 #include "../Heders/Cargo.h"
+//getters
 
+    std::string const Cargo::getName(){return name_;};
+    int const Cargo::getAmount(){return amount_;};
+    float const Cargo::getBasicPrice(){return basicPrice_;};
+    int const Cargo::getCargoWeight(){return cargoWeight_;};
 
 //operatorts
 
